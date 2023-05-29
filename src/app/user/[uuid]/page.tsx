de43572f6b204a0f4b366834dd6e0a47e7997e7f
FluffyCuteOwO <admjgptw@yandex.ru>
2023-05-29 10:25:26 +0300

    syncronize with figma

--- src/app/user/[uuid]/page.tsx
+++ src/app/user/[uuid]/page.tsx
@@ -19,7 +19,7 @@ export default async function User({ params }: any) {
                 <svg width="2" height="22" viewBox="0 0 2 22" fill="none" xmlns="http://www.w3.org/2000/svg">
                     <path d="M1 1L1 21" stroke="#333333" strokeLinecap="round" />
                 </svg>
-                <p>Просмотр резюме студента - {student.fullName}</p>
+                <p>Просмотр резюме - {student.fullName}</p>
             </header>
             <div>
                 <section>
